076f5a7a623d484baac1f78e5017cad2f4326ef5
xsnieh00 <@>
2024-03-25 19:54:36 +0100

    fix: assignment of int values to chars

--- src/msg_compose.c
+++ src/msg_compose.c
@@ -7,7 +7,7 @@
 
 #include "msg_compose.h"
 
-int compose(t_msg msg, t_string * send, int * len) {
+int compose(t_msg msg, t_string * send, int * len) { 
     switch (msg.type) {
         case e_confirm: return compose_confirm(msg, send, len); break;
         case e_reply: return compose_reply(msg, send, len); break;
@@ -24,9 +24,9 @@ int compose(t_msg msg, t_string * send, int * len) {
 
 int compose_confirm(t_msg msg, t_string * send, int * len) {
     *len = 3;
-    (*send)[0] = e_confirm;
-    (*send)[1] = (msg.content.confirm.ref_id / 10) % 10;
-    (*send)[2] = msg.content.confirm.ref_id % 10;
+    sprintf((*send)+0*sizeof(char), "%d", e_confirm);
+    sprintf((*send)+1*sizeof(char), "%d", (msg.content.confirm.ref_id / 10) % 10);
+    sprintf((*send)+2*sizeof(char), "%d", msg.content.confirm.ref_id % 10);
 
     return 0;
 }
@@ -34,16 +34,16 @@ int compose_confirm(t_msg msg, t_string * send, int * len) {
 int compose_reply(t_msg msg, t_string * send, int * len) {
     int index;
     int added_len = 0;
-    (*send)[0] = e_reply;
-    (*send)[1] = (msg.id / 10) % 10;
-    (*send)[2] = msg.id % 10;
-    (*send)[3] = msg.content.reply.result;
-    (*send)[4] = (msg.content.reply.ref_id / 10) % 10;
-    (*send)[5] = msg.content.reply.ref_id % 10;
+    sprintf((*send)+0*sizeof(char), "%d", e_reply);
+    sprintf((*send)+1*sizeof(char), "%d", (msg.id / 10) % 10);
+    sprintf((*send)+2*sizeof(char), "%d", msg.id % 10);
+    sprintf((*send)+3*sizeof(char), "%d", msg.content.reply.result);
+    sprintf((*send)+4*sizeof(char), "%d", (msg.content.reply.ref_id / 10) % 10);
+    sprintf((*send)+5*sizeof(char), "%d", msg.content.reply.ref_id % 10);
     index = 6;
     compose_string(&(*send),msg.content.reply.content, index, &added_len);
     index += added_len;
-    (*send)[++index] = 0x00;
+    (*send)[index++] = 0x00;
     *len = index;
 
     return 0;
@@ -52,19 +52,19 @@ int compose_reply(t_msg msg, t_string * send, int * len) {
 int compose_auth(t_msg msg, t_string * send, int * len) {
     int index;
     int added_len = 0;
-    (*send)[0] = e_auth;
-    (*send)[1] = (msg.id / 10) % 10;
-    (*send)[2] = msg.id % 10;
+    sprintf((*send)+0*sizeof(char), "%d", e_auth);
+    sprintf((*send)+1*sizeof(char), "%d", (msg.id / 10) % 10);
+    sprintf((*send)+2*sizeof(char), "%d", msg.id % 10);
     index = 3;
-    compose_string(&(*send),msg.content.auth.user_name, index, &added_len);
+    compose_string(send,msg.content.auth.user_name, index, &added_len);
     index += added_len;
-    (*send)[++index] = 0x00;
+    (*send)[index++] = 0x00;
     added_len = 0;
-    compose_string(&(*send),msg.content.auth.display_name, index, &added_len);
+    compose_string(send, msg.content.auth.display_name, index, &added_len);
     index += added_len;
-    (*send)[++index] = 0x00;
+    (*send)[index++] = 0x00;
     added_len = 0;
-    compose_string(&(*send),msg.content.auth.secret, index, &added_len);
+    compose_string(send,msg.content.auth.secret, index, &added_len);
     index += added_len;
     *len = index;
 
@@ -74,17 +74,17 @@ int compose_auth(t_msg msg, t_string * send, int * len) {
 int compose_join(t_msg msg, t_string * send, int * len) {
     int index;
     int added_len = 0;
-    (*send)[0] = e_join;
-    (*send)[1] = (msg.id / 10) % 10;
-    (*send)[2] = msg.id % 10;
+    sprintf((*send)+0*sizeof(char), "%d", e_join);
+    sprintf((*send)+1*sizeof(char), "%d", (msg.id / 10) % 10);
+    sprintf((*send)+2*sizeof(char), "%d", msg.id % 10);
     index = 3;
     compose_string(&(*send),msg.content.join.channel_id, index, &added_len);
     index += added_len;
-    (*send)[++index] = 0x00;
+    (*send)[index++] = 0x00;
     added_len = 0;
     compose_string(&(*send),msg.content.join.display_name, index, &added_len);
     index += added_len;
-    (*send)[++index] = 0x00;
+    (*send)[index++] = 0x00;
     *len = index;
 
     return 0;
@@ -93,17 +93,17 @@ int compose_join(t_msg msg, t_string * send, int * len) {
 int compose_msg(t_msg msg, t_string * send, int * len) {
     int index;
     int added_len = 0;
-    (*send)[0] = e_msg;
-    (*send)[1] = (msg.id / 10) % 10;
-    (*send)[2] = msg.id % 10;
+    sprintf((*send)+0*sizeof(char), "%d", e_msg);
+    sprintf((*send)+1*sizeof(char), "%d", (msg.id / 10) % 10);
+    sprintf((*send)+2*sizeof(char), "%d", msg.id % 10);
     index = 3;
     compose_string(&(*send),msg.content.msg.display_name, index, &added_len);
     index += added_len;
-    (*send)[++index] = 0x00;
+    (*send)[index++] = 0x00;
     added_len = 0;
     compose_string(&(*send),msg.content.msg.display_name, index, &added_len);
     index += added_len;
-    (*send)[++index] = 0x00;
+    (*send)[index++] = 0x00;
     *len = index;
 
     return 0;
@@ -112,17 +112,17 @@ int compose_msg(t_msg msg, t_string * send, int * len) {
 int compose_err(t_msg msg, t_string * send, int * len) {
     int index;
     int added_len = 0;
-    (*send)[0] = e_err;
-    (*send)[1] = (msg.id / 10) % 10;
-    (*send)[2] = msg.id % 10;
+    sprintf((*send)+0*sizeof(char), "%d", e_err);
+    sprintf((*send)+1*sizeof(char), "%d", (msg.id / 10) % 10);
+    sprintf((*send)+2*sizeof(char), "%d", msg.id % 10);
     index = 3;
     compose_string(&(*send),msg.content.err.display_name, index, &added_len);
     index += added_len;
-    (*send)[++index] = 0x00;
+    (*send)[index++] = 0x00;
     added_len = 0;
     compose_string(&(*send),msg.content.err.display_name, index, &added_len);
     index += added_len;
-    (*send)[++index] = 0x00;
+    (*send)[index++] = 0x00;
     *len = index;
 
     return 0;
@@ -130,19 +130,17 @@ int compose_err(t_msg msg, t_string * send, int * len) {
 
 int compose_bye(t_msg msg, t_string * send, int * len) {
     *len = 3;
-    (*send)[0] = e_bye;
-    (*send)[1] = (msg.id / 10) % 10;
-    (*send)[2] = msg.id % 10;
+    sprintf((*send)+0*sizeof(char), "%d", e_bye);
+    sprintf((*send)+1*sizeof(char), "%d", (msg.id / 10) % 10);
+    sprintf((*send)+2*sizeof(char), "%d", msg.id % 10);
 
     return 0;
 }
 
 int compose_string(t_string * dst, t_string src, int index, int * len) {
     int i = 0;
-    while (src[index] != 0x00) {
-        (*dst)[i] = src[index];
-        i++;
-        index++;
+    while (src[i] != 0x00) {
+        (*dst)[index++] = src[i++];
     }
     *len = i;
 
