470362138f8fb42719da1a5e660e5947d8d8fe2f
xsnieh00 <@>
2024-03-26 02:55:21 +0100

    feat: state handling

---
+++ src/state.c
@@ -0,0 +1,56 @@
+/**
+ * @file state.c
+ * @author Sniehovskyi Nikita (xsnieh00)
+ * @date 16.03.2024
+ * @brief Resolves states operation of automata
+ */
+
+#include "state.h"
+
+int get_next_state(int cur_state, t_msg msg) {
+    switch (cur_state) {
+        case e_state_null: return e_state_null; break;
+        case e_state_start: return next_state_start(msg.type); break;
+        case e_state_auth: return next_state_auth(msg.type, msg.content.reply.result); break;
+        case e_state_open: return next_state_open(msg.type); break;
+        case e_state_err: return next_state_err(msg.type); break;
+        case e_state_end: return next_state_end(msg.type); break; 
+    }
+    return e_state_null;
+}
+
+int next_state_start(int msg_type) {
+    switch(msg_type) {
+        case e_auth: return e_state_auth; break; 
+    }
+    return e_state_null;
+}
+
+int next_state_auth(int msg_type, bool is_reply) {
+    switch(msg_type) {
+        case e_reply: if (is_reply) { return e_state_open; } else { return e_state_auth; } break; 
+        case e_auth: return e_state_auth; break; 
+        case e_err: return e_state_end; break; 
+        case e_bye: return e_state_end; break; 
+    }
+    return e_state_null;
+}
+
+int next_state_open(int msg_type) {
+    switch(msg_type) {
+        case e_reply: return e_state_open; break; 
+        case e_join: return e_state_open; break; 
+        case e_msg: return e_state_open; break; 
+        case e_err: return e_state_end; break; 
+        case e_bye: return e_state_end; break; 
+    }
+    return e_state_null;
+}
+
+int next_state_err(int msg_type) {
+    return e_state_end;
+}
+
+int next_state_end(int msg_type) {
+    return e_state_end;
+}
---
+++ src/state.h
@@ -0,0 +1,68 @@
+/**
+ * @file command.h
+ * @author Sniehovskyi Nikita (xsnieh00)
+ * @date 26.03.2024
+ * @brief Header file for state handling
+ *
+ * Resolves states operation of automata
+ */
+
+#ifndef __STATE_H__
+
+#define __STATE_H__
+
+#include <stdbool.h>
+
+#include "types.h"
+
+/**
+ * @brief Gets the next state
+ * 
+ * @param cur_state Current state
+ * @param msg Received/pending message
+ * @return int New state
+ */
+int get_next_state(int cur_state, t_msg msg);
+
+/**
+ * @brief Gets next state after start
+ * 
+ * @param msg_type Type of received/pending message
+ * @return int New state
+ */
+int next_state_start(int msg_type);
+
+/**
+ * @brief Gets next state after auth
+ * 
+ * @param msg_type Type of received/pending message
+ * @param is_reply Is reply positive (ignored if no need)
+ * @return int New state
+ */
+int next_state_auth(int msg_type, bool is_reply);
+
+/**
+ * @brief Gets next state after open
+ * 
+ * @param msg_type Type of received/pending message
+ * @return int New state
+ */
+int next_state_open(int msg_type);
+
+/**
+ * @brief Gets next state after err
+ * 
+ * @param msg_type Type of received/pending message
+ * @return int New state
+ */
+int next_state_err(int msg_type);
+
+/**
+ * @brief Gets next state after end
+ * 
+ * @param msg_type Type of received/pending message
+ * @return int New state
+ */
+int next_state_end(int msg_type);
+
+#endif
